3e18e940f64a5dd150fd161e2e7c9de7056e31e6
SHlee5433 <tmdgktqhdl@naver.com>
2024-08-30 18:42:45 +0900

    function update

--- code/function.py
+++ code/function.py
@@ -4,24 +4,27 @@ def g(x=3):
 
 g()
 
-# 함수 내용 확인
+print(g)
+
+# 함수 내용확인
 import inspect
+
 print(inspect.getsource(g))
 
 import numpy as np
 
 np.array([1, 2, 3])
 
-# if else 정식 구문
+# if .. else 정식
 x = 3
 if x > 4:
- y = 1
+    y = 1
 else:
- y = 2
+    y = 2
 print(y)
 
 # if else 축약
-y = 1 if  x > 4 else 2
+y = 1 if x > 4 else 2
 y
 
 # 리스트 컴프리헨션
@@ -44,38 +47,33 @@ import numpy as np
 
 x = np.array([1, -2, 3, -4, 0])
 conditions = [x > 0, x == 0, x < 0]
-choices = ["양수", "0", "음수"]
+choices    = ["양수", "0", "음수"]
 result = np.select(conditions, choices, x)
 print(result)
 
 
-
 # for loop
 for i in range(1, 4):
-   print(f"Here is {i}")
+    print(f"Here is {i}")
 
 # for loop 리스트 컴프
 [f"Here is {i}" for i in range(1, 4)]
 
 
 name = "남규"
-age = "31(진)"
-greeting = f"Hello, my name is {name} and I am {age} years old."
-print(greeting)
+age = "31 (진)"
+f"Hello, my name is {name} and I am {age} years old."
 
 import numpy as np
 
 names = ["John", "Alice"]
-ages = np.array([25, 30]) # 나이 배열의 길이를 names 리스트와 맞춤
+ages = np.array([25, 30])
 
-# 각 이름과 나이에 대해 별도로 인사말 생성
-greetings = [f"이름 {name} and I am {age} years old." for name, age
-in zip(names, ages)]
-for greeting in greetings:
-    print(greeting)
- 
+greetings = [f"이름: {name}, 나이: {age}." for name, age in zip(names, ages)]
 
+# zip() 함수 활용법
 import numpy as np
+
 names = ["John", "Alice"]
 ages = np.array([25, 30])
 
@@ -90,84 +88,91 @@ for name, age in zipped:
 # while 문
 i = 0
 while i <= 10:
-   i += 3
-   print(i)
+    i += 3
+    print(i)
 
 # while, break 문
 i = 0
 while True:
-   i += 3
-   if i > 10:
-      break
-   print(i)
+    i += 3
+    if i > 10:
+        break
+    print(i)
 
-
+# apply
 import pandas as pd
+
 data = pd.DataFrame({
-   "A" : [1, 2, 3],
-   "B" : [4, 5, 6]
-})
+    'A': [1, 2, 3], 
+    'B': [4, 5, 6]
+    })
 data
 
-data.apply(max, axis = 0)
-data.apply(max, axis = 1)
+data.apply(sum, axis=0)
+data.apply(max, axis=1)
 
-def my_func(x, const = 3):
-   return max(x) ** 2 + const
+def my_func(x, const=3):
+    return max(x)**2 + const
 
 my_func([3, 4, 10], 5)
 
-data.apply(my_func, axis = 0, const = 5)
+data.apply(my_func, axis=0, const=5)
+
+
+import numpy as np
 
 array_2d = np.arange(1, 13).reshape((3, 4), order='F')
 print(array_2d)
 
-np.apply_along_axis(max, axis = 0,
-                    arr=array_2d)
+np.apply_along_axis(max, axis=0, arr=array_2d)
 
 # 함수 환경
 
 y = 2
 
 def my_func(x):
-   global y
+    global y
 
-   def my_f(k):
-      return K ** 2
-   
-   y = my_f(k) + 1
-   result = x + y
-   return result
+    def my_f(k):
+        return k**2
+
+    y = my_f(x) + 1
+    result = x + y
+
+    return result
+
+
+my_f(3)
 
 my_func(3)
 print(y)
 
-# 입력값이 몇 개인지 모를 땐 *
+# 입력값이 몇 개일지 모를땐 별표를 * 붙임
 def add_many(*args): 
-     result = 0 
-     for i in args: 
-         result = result + i   # *args에 입력받은 모든 값을 더한다.
+    result = 0
+    for i in args: 
+        result = result + i
 
-     return result 
+    return result
 
 add_many(1, 2, 3)
 
-def first_many(*args):
-   return args[0]
+def first_many(*args): 
+    return args[0]
 
 first_many(1, 2, 3)
 first_many(4, 1, 2, 3)
 
-def add_mul(choice, *args):
+def add_mul(choice, *my_input): 
     if choice == "add":
-        result = 0
-        for i in args:
-            result += i
+        result = 0 
+        for i in my_input: 
+            result = result + i 
     elif choice == "mul":
-        result = 1
-        for i in args:
-            result *= i
-    return result
+        result = 1 
+        for i in my_input: 
+            result = result * i 
+    return result 
 
 add_mul("mul", 5, 4, 3, 1)
 
